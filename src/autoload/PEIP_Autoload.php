f16c05d46015412dbdb2e0320a70077c4c07b09c
Timo Michna <timomichna@yahoo.de>
2010-04-01 18:39:36 +0200

    added doctags to autoloading classes

--- src/autoload/PEIP_Autoload.php
+++ src/autoload/PEIP_Autoload.php
@@ -1,19 +1,55 @@
 <?php
 require_once(__DIR__.'/PEIP_Simple_Autoload.php');
 
+/*
+ * This file is part of the PEIP package.
+ * (c) 2010 Timo Michna <timomichna/yahoo.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * PEIP_Simple_Autoload 
+ * Extended Autoloading class. Loads class-paths from 
+ * PEIP_Autoload_Paths::$paths on startup.
+ * Can (re)generate PEIP_Autoload_Paths::$paths through method 'make'.
+ * 
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP 
+ * @subpackage autoload 
+ * @extends PEIP_Simple_Autoload
+ */
 
 class PEIP_Autoload extends PEIP_Simple_Autoload {
-
-	public function __construct(){
+	
+    /**
+     * Constructor.
+     * Loads class-paths from PEIP_Autoload_Paths::$paths
+     * 
+     * @access protected
+     */	
+	protected function __construct(){
 		$this->init();
 		require_once(__DIR__.'/PEIP_Autoload_Paths.php');
 		$this->setClassPaths(PEIP_Autoload_Paths::$paths);
 	}
 	
+    /**
+     * @access protected
+     * @return PEIP_Autoload
+     */
 	protected static function doGetInstance(){
-		self::$instance = new PEIP_Autoload();	
+		return self::$instance = new PEIP_Autoload();	
 	}		
-	
+
+    /**
+     * Regenerates the class/files associations and replaces them in PEIP_Autoload_Paths
+     * 
+     * @access public
+     * @static
+     * @return void
+     */	
 	public static function make(){
 		$baseDir = str_replace(DIRECTORY_SEPARATOR, '/', realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'));
 		$iterator = new RecursiveDirectoryIterator($baseDir);
@@ -24,7 +60,17 @@ class PEIP_Autoload extends PEIP_Simple_Autoload {
 		echo $content = preg_replace('/public static \$paths = array *\(.*?\);/s', sprintf("public static \$paths = %s;", var_export($paths, true)), $content);
 		file_put_contents($pathsFile, $content);
 	}
-	 
+
+    /**
+     * Traverses a directory to find class-files
+     * 
+     * @access protected
+     * @static
+     * @param string $baseDir the directory under which to look for classes.
+     * @param RecursiveDirectoryIterator $iterator A RecursiveDirectoryIterator instance for the directory
+     * @param array $paths the array to store the class/path associations in.
+     * @return PEIP_Autoload
+     */	
 	protected static function findPaths($baseDir, RecursiveDirectoryIterator $iterator, array $paths = array()){
 		$iterator->rewind();
 		while($iterator->valid()){
--- src/autoload/PEIP_Simple_Autoload.php
+++ src/autoload/PEIP_Simple_Autoload.php
@@ -9,7 +9,8 @@
 
 /**
  * PEIP_Simple_Autoload 
- *
+ * Class providing basic autoload features
+ * 
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
  * @subpackage autoload 
@@ -20,14 +21,25 @@ class PEIP_Simple_Autoload {
 	protected
     	$baseDir = null,
     	$classPaths = array();
-
+	
   	static protected
     	$instance;    	
     	
+    /**
+     * Constructor.
+     * 
+     * @access protected
+     */	        	
 	protected function __construct(){
 		$this->init();
 	}	
 
+    /**
+     * Initialization method.
+     * Registers autoload with this class.
+     * 
+     * @access protected
+     */	 	
 	protected function init(){
 		$this->baseDir = realpath(dirname(__FILE__).'/..');
 	    ini_set('unserialize_callback_func', 'spl_autoload_call');
@@ -35,7 +47,14 @@ class PEIP_Simple_Autoload {
       		throw new RuntimeException(sprintf('Unable to register %s::autoload as an autoloading method.', get_class($this)));
     	}	
 	}
-	
+
+    /**
+     * Retrieves Singleton instance 
+     * 
+     * @access public
+     * @static
+     * @return PEIP_Simple_Autoload
+     */		
 	static public function getInstance(){
 	    if (!isset(self::$instance)){
 	      static::doGetInstance();
@@ -43,25 +62,61 @@ class PEIP_Simple_Autoload {
 	    return self::$instance;
 	}
 
+    /**
+     * Retrieves Singleton instance
+     * To be overwritten by subclasses 
+     * 
+     * @access protected
+     * @return PEIP_Simple_Autoload
+     */	
 	protected static function doGetInstance(){
-		self::$instance = new PEIP_Simple_Autoload();;	
+		return self::$instance = new PEIP_Simple_Autoload();;	
 	}
-	
+		
+    /**
+     * Sets a single file-path for a class
+     * 
+     * @access public
+     * @param string $class the class name
+     * @param string $path the path to the class-file  
+     * @return void
+     */		
 	public function setClassPath($class, $path){
 		$this->classPaths[$class] = $path;
 	}
-	
+
+    /**
+     * Sets all class-paths as key(class)/value(path) pairs array
+     * 
+     * @access public
+     * @param array $classPaths class-paths as key(class)/value(path) pairs array
+     * @return void
+     */		
 	public function setClassPaths(array $classPaths){
 		$this->classPaths = $classPaths;	
 	}	
 
+    /**
+     * returns the path to a class-file (if registered)
+     * 
+     * @access public
+     * @param string $class the class name
+     * @return path to the class-file 
+     */		
 	public function getClassPath($class){
    	 	if (!isset($this->classPaths[$class])){
       		return null;
     	}
     	return $this->baseDir.'/'.$this->classPaths[$class];
 	}	
-
+	
+    /**
+     * handles the autoloading of classes
+     * 
+     * @access public
+     * @param string $class the class to load
+     * @return path to the class-file 
+     */		
   	public function autoload($class){
   		if ($path = $this->getClassPath($class)){
   			require $path;
@@ -71,8 +126,6 @@ class PEIP_Simple_Autoload {
     	}
     	return false;
   	}	
-	
-	
 }
 
 
