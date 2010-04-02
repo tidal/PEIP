35e61ee2ec8a10de85938840f85e0dd0680ba660
Timo Michna <timomichna@yahoo.de>
2010-04-02 16:39:16 +0200

    added Test Suites

---
+++ tests/AllTests.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__.'/service/AllTests.php';
+require_once __DIR__.'/channel/AllTests.php';
+require_once __DIR__.'/command/AllTests.php';
+require_once __DIR__.'/data/AllTests.php';
+require_once __DIR__.'/base/AllTests.php';
+require_once __DIR__.'/message/AllTests.php';
+
+ class AllTests extends PHPUnit_Framework_TestSuite {
+	 public static function suite(){
+		$suite = new PHPUnit_Framework_TestSuite('');
+		$suite->addTestSuite('Service_AllTests');
+		$suite->addTestSuite('Channel_AllTests');
+		$suite->addTestSuite('Command_AllTests');
+		$suite->addTestSuite('Data_AllTests');
+		$suite->addTestSuite('Base_AllTests');
+		$suite->addTestSuite('Message_AllTests');
+		return $suite;
+	}
+}
