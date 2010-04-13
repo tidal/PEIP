7c610b3ff77fee969ac4db26d0c93b15c7900616
Timo Michna <timomichna@yahoo.de>
2010-04-13 21:31:03 +0200

    added test for PEIP_Object_Event_Dispatcher

--- tests/dispatcher/AllTests.php
+++ tests/dispatcher/AllTests.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once __DIR__.'/ObjectMapDispatcherTest.php';
+require_once __DIR__.'/ObjectEventDispatcherTest.php';
 
  class Dispatcher_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
 		$suite = new PHPUnit_Framework_TestSuite('dispatcher');
 		$suite->addTestSuite('ObjectMapDispatcherTest');
+		$suite->addTestSuite('ObjectEventDispatcherTest');
 		return $suite;
 	}
 }
