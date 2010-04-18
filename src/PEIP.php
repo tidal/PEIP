c45091173c0a74b46371ea3e932ace6e3f8bad00
Timo Michna <timomichna@yahoo.de>
2010-04-18 16:24:29 +0000

    added package loader

--- src/PEIP.php
+++ src/PEIP.php
@@ -5,6 +5,12 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+/**
+ * PEIP 
+ * PEIP package loader
+ * 
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP 
+ */
 require_once(__DIR__.'/autoload/PEIP_Autoload.php');
 PEIP_Autoload::getInstance();
