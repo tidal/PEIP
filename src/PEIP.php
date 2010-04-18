95613d50f034703c94038da690b28b620b8d276f
Timo Michna <timomichna@yahoo.de>
2010-04-18 16:12:23 +0000

    added PEIP.php

---
+++ src/PEIP.php
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the PEIP package.
+ * (c) 2010 Timo Michna <timomichna/yahoo.de>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once(__DIR__.'/autoload/PEIP_Autoload.php');
+PEIP_Autoload::getInstance();
