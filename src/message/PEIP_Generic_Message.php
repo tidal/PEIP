33c8940205b09386bf77e754412a09cec5d57026
Timo Michna <timomichna@yahoo.de>
2010-05-03 14:33:11 +0000

    [PEIP_Generic_Message] doc-tags and inline documentation

--- src/message/PEIP_Generic_Message.php
+++ src/message/PEIP_Generic_Message.php
@@ -27,13 +27,13 @@ class PEIP_Generic_Message
     protected $payload;
     
     protected $headers; 
-        
-    
+           
     /**
+     * constructor
+     * 
      * @access public
-     * @param $content 
-     * @param $headers 
-     * @return 
+     * @param mixed $content The content/payload of the message 
+     * @param ArrayAccess $headers ArrayAccess object of headers as key/value pairs
      */
     public function __construct($content, ArrayAccess $headers = NULL){
         $this->doSetContent($content);      
@@ -41,48 +41,60 @@ class PEIP_Generic_Message
             $this->headers = $headers;
         }           
     }
-
-    
+  
     /**
+     * sets content/payload of message - to be overwritten by derived classes
+     * 
      * @access protected
-     * @param $content 
-     * @return 
+     * @param mixed $content The content/payload of the message 
      */
     protected function doSetContent($content){
         $this->content = $content;
     }
     
     /**
+     * returns all headers of the message
+     * 
      * @access public
-     * @param $name 
-     * @return 
+     * @return ArrayAccess ArrayAccess object of headers
      */
     public function getHeaders(){
         return $this->headers;
     }
-
-    
+  
     /**
+     * returns one specific header of the message
+     * 
      * @access public
-     * @param $name 
-     * @return 
+     * @param string $name the name of the header  
+     * @return mixed the value of the header
      */
     public function getHeader($name){
         return $this->headers[$name];
     }
-    
-    
+     
     /**
+     * checks wether a specific header is set on the message
+     * 
      * @access public
-     * @param $name 
-     * @return 
+     * @param string $name the name of the header
+     * @return boolean wether the header is set
      */
     public function hasHeader($name){
         return isset($this->headers[$name]);
     }
-    
+   
+    /**
+     * Provides a static build method to create new Instances of this class.
+     * Implements PEIP_INF_Buildable
+     * 
+     * @static
+     * @access public
+     * @implements PEIP_INF_Buildable
+     * @param string $name the name of the header
+     * @return boolean wether the header is set
+     */    
     public static function build(array $arguments = array()){
         return PEIP_Generic_Builder::getInstance('PEIP_Generic_Message')->build($arguments);    
-    }   
-    
+    }     
 } 
