6626311468012262b70ec4ae229a91b5ddf5be7d
Timo Michna <timomichna@yahoo.de>
2010-05-05 17:54:24 +0000

    [PEIP_ABS_Dispatcher] extended inline documentation

--- src/ABS/dispatcher/PEIP_ABS_Dispatcher.php
+++ src/ABS/dispatcher/PEIP_ABS_Dispatcher.php
@@ -10,22 +10,39 @@
 
 /**
  * PEIP_ABS_Dispatcher 
+ * Abstract dispatcher base class.
  *
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
  * @subpackage dispatcher 
  */
 
-
 abstract class PEIP_ABS_Dispatcher {
-
-    
+	
+    /**
+     * notifies given listeners of given subject
+     * 
+     * @access protected
+     * @static 
+     * @param array array of PEIP_INF_Handler instaces
+     * @param mixed $subject subject to notify the listeners of 
+     * @return void
+     */	
     protected static function doNotify(array $listeners, $subject){
         foreach($listeners as $listener){
             $listener->handle($subject);    
         }   
     }  
 
+    /**
+     * notifies given listeners on a subject until one returns a boolean true value
+     * 
+     * @access protected
+     * @static 
+     * @param array array of PEIP_INF_Handler instaces
+     * @param mixed $subject subject to notify the listeners of 
+     * @return PEIP_INF_Handler listener that returned a boolean true value
+     */	    
     protected static function doNotifyUntil(array $listeners, $subject){
         foreach ($listeners as $listener){
           if ($listener->handle($subject)){
@@ -33,6 +50,5 @@ abstract class PEIP_ABS_Dispatcher {
           }
         }
     } 
-
 
 }
