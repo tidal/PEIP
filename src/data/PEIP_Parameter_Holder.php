56c381f4fb229d149f000724f69d3df429f47605
Timo Michna <timomichna@yahoo.de>
2010-05-05 18:01:17 +0000

    [PEIP_Parameter_Holder] extended inline documentation

--- src/data/PEIP_Parameter_Holder.php
+++ src/data/PEIP_Parameter_Holder.php
@@ -10,6 +10,8 @@
 
 /**
  * PEIP_Parameter_Holder 
+ * Class to act as a simple holder for parameters as key/value pairs.
+ * Can act as a base class for classes dealing with parameters.
  *
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
@@ -17,109 +19,96 @@
  * @implements PEIP_INF_Parameter_Holder
  */
 
-
-
 class PEIP_Parameter_Holder 
     implements PEIP_INF_Parameter_Holder {
 
     protected $parameters = array();
-    
-    
-    
+        
     /**
+     * Checks wether a given parameter is set
+     * 
      * @access public
-     * @param $key 
-     * @return 
+     * @param string $key name of parameter
+     * @return boolean wether parameter is set
      */
     public function hasParameter($key){
         return array_key_exists($key, $this->parameters);
     }   
-    
-    
+        
     /**
+     * returns a given parameter value
+     * 
      * @access public
-     * @param $key 
+     * @param string $key name of parameter 
      * @return 
      */
     public function getParameter($key){
         return array_key_exists($key, $this->parameters) ? $this->parameters[$key] : NULL;
     }
-    
-    
+        
     /**
+     * Deletes a given parameter
+     * 
      * @access public
-     * @param $key 
+     * @param string $key name of parameter 
      * @return 
      */
     public function deleteParameter($key){
         unset($this->parameters[$key]);
     }   
-
-    
+   
     /**
+     * Sets the value for a given parameter
+     * 
      * @access public
-     * @param $key 
-     * @param $value 
+     * @param string $key name of parameter 
+     * @param string $value value to set for the parameter 
      * @return 
      */
     public function setParameter($key, $value){
         $this->parameters[$key] = $value;
     }
     
-    
     /**
+     * Sets all parameters as key/value pairs
+     * 
      * @access public
-     * @param $key 
-     * @param $value 
-     * @return 
-     */
-    
-    /**
-     * @access public
-     * @param $parameters 
+     * @param array $parameters parameters as key/value pairs 
      * @return 
      */
     public function setParameters(array $parameters){
         $this->parameters = $parameters;        
     }
-
-    
+   
     /**
+     * returns parameters as key/value pairs
+     * 
      * @access public
-     * @param $key 
-     * @return 
-     */
-    
-    /**
-     * @access public
-     * @return 
+     * @return array $parameters parameters as key/value pairs
      */
     public function getParameters(){
         return $this->parameters;
     }
-
-    
+   
     /**
+     * Adds parameters as key/value pairs.
+     * Overwrites value for a parameter if allready been set.
+     * 
      * @access public
-     * @param $parameters 
+     * @param array $parameters parameters as key/value pairs 
      * @return 
      */
     public function addParameters(array $parameters){
         $this->parameters = array_merge($this->parameters, $parameters);
         array_unique($this->parameters);
     }   
-
-
     
     /**
+     * Adds parameters as key/value pairs only if parameter has not allready
+     * been set.
+     * 
      * @access public
-     * @param $parameters 
-     * @return 
-     */
-    
-    /**
-     * @access public
-     * @param $parameters 
+     * @param array $parameters parameters as key/value pairs 
      * @return 
      */
     public function addParametersIfNot(array $parameters){
