5dc348531adaae28b472687e19496565f1d3ecef
Timo Michna <timomichna@yahoo.de>
2010-05-05 17:58:38 +0000

    [PEIP_Reflection_Class_Builder] extended inline documentation

--- src/base/PEIP_Reflection_Class_Builder.php
+++ src/base/PEIP_Reflection_Class_Builder.php
@@ -10,6 +10,7 @@
 
 /**
  * PEIP_Reflection_Class_Builder 
+ * Provides a fluid interface to create ReflectionClass instances
  *
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
@@ -17,18 +18,32 @@
  * @implements PEIP_INF_Singleton_Map
  */
 
-
 class PEIP_Reflection_Class_Builder implements PEIP_INF_Singleton_Map {
 
     protected static $reflectionClasses = array();
-    
+       
+    /**
+     * Returns instance of ReflectionClass for given class
+     * 
+     * @access public
+     * @param string $className name of the class to return ReflectionClass for 
+     * @return ReflectionClass the ReflectionClass instance for given class
+     */    
     protected static function getReflectionClass($className){
         return array_key_exists($className, self::$reflectionClasses) 
             ? self::$reflectionClasses[$className] 
             : (self::$reflectionClasses[$className] = new ReflectionClass($className)); 
     }   
     
-    
+    /**
+     * Returns instance of ReflectionClass for given class.
+     * Implements PEIP_INF_Singleton_Map
+     * 
+     * @implements PEIP_INF_Singleton_Map
+     * @access public
+     * @param string $className name of the class to return ReflectionClass for 
+     * @return ReflectionClass the ReflectionClass instance for given class
+     */     
     public static function getInstance($className){
         return self::getReflectionClass($className);
     }
