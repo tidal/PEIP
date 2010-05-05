5ff63e7ec708d5bbd2f5f7378c4cb5a54882fb3b
Timo Michna <timomichna@yahoo.de>
2010-05-05 17:59:35 +0000

    [PEIP_Pollable_Channel] extended inline documentation

--- src/channel/PEIP_Pollable_Channel.php
+++ src/channel/PEIP_Pollable_Channel.php
@@ -10,6 +10,7 @@
 
 /**
  * PEIP_Pollable_Channel 
+ * Basic concete implementation of a pollable channel
  *
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
@@ -18,30 +19,31 @@
  * @implements PEIP_INF_Connectable, PEIP_INF_Interceptable, PEIP_INF_Channel, PEIP_INF_Pollable_Channel
  */
 
-
-
-
 class PEIP_Pollable_Channel  
     extends PEIP_ABS_Interceptable_Message_Channel 
     implements PEIP_INF_Pollable_Channel {
 
     protected 
         $messages = array();
-        
-    
+          
     /**
+     * Sends a message on the channel.
+     * 
      * @access protected
-     * @param $message 
+     * @param PEIP_INF_Message $message the message to send
      * @return 
      */
     protected function doSend(PEIP_INF_Message $message){
         $this->messages[] = $message;        
     }
-    
-    
+      
     /**
+     * Receives a message from the channel
+     * 
+     * @event preReceive
+     * @event postReceive
      * @access public
-     * @param $timeout 
+     * @param integer $timeout timout for receiving a message 
      * @return 
      */
     public function receive($timeout = -1){
@@ -64,30 +66,33 @@ class PEIP_Pollable_Channel
         $this->doFireEvent('postReceive', array('MESSAGE'=>$message));
         return $message;
     }
-
-    
+   
     /**
+     * Returns a message from top of the message stack
+     * 
      * @access protected
-     * @return 
+     * @return PEIP_INF_Message message from top of the message stack
      */
     protected function getMessage(){
         return array_shift($this->messages);
     }
-    
-    
+       
     /**
+     * Deletes all messages on the message stack.
+     * 
      * @access public
      * @return 
      */
     public function clear(){
         $this->messages = array();
     }
-    
-    
+        
     /**
+     * Removes all messages not accepted by a given message-selector from the message-stack.
+     * 
      * @access public
-     * @param $selector 
-     * @return 
+     * @param PEIP_INF_Message_Selector $selector the selector to accept messages 
+     * @return array accepted messages
      */
     public function purge(PEIP_INF_Message_Selector $selector){
         foreach($this->messages as $key=>$message){
@@ -97,6 +102,5 @@ class PEIP_Pollable_Channel
         }
         return $this->messages;
     }
-        
-    
+           
 }
