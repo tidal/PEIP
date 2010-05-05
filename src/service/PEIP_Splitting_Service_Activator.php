6e29080f85927002d22d16a07aeb4172bf5ec377
Timo Michna <timomichna@yahoo.de>
2010-05-05 18:07:58 +0000

    [PEIP_Splitting_Service_Activator] extended inline documentation

--- src/service/PEIP_Splitting_Service_Activator.php
+++ src/service/PEIP_Splitting_Service_Activator.php
@@ -10,7 +10,7 @@
 /**
  * PEIP_Service_Activator 
  * 
- * Calls the service method with the content of the Message as arguments
+ * Calls the service method with the content of the Message (array) as arguments
  *
  * @author Timo Michna <timomichna/yahoo.de>
  * @package PEIP 
@@ -19,15 +19,16 @@
  * @implements PEIP_INF_Message_Builder, PEIP_INF_Handler, PEIP_INF_Channel, PEIP_INF_Subscribable_Channel, PEIP_INF_Connectable
  */
 
-
 class PEIP_Splitting_Service_Activator
     extends PEIP_Service_Activator {
-            
-    
+              
     /**
-     * @access public
-     * @param $message 
-     * @return 
+     * Calls a method on a service (registered as a callable) with 
+     * content/payload (array) of given message as arguments.
+     * 
+     * @access protected
+     * @param PEIP_INF_Message $message message to call the service with it´s content/payload
+     * @return mixed result of calling the registered service callable with message content/payload
      */
     public function callService(PEIP_INF_Message $message){
         if(is_callable($this->serviceCallable)){
@@ -37,6 +38,5 @@ class PEIP_Splitting_Service_Activator
         }
         return $res;
     } 
-    	
-    
+    	   
 }
