ac46895e027eaf34186ded4348e27c20b2ea43d7
Timo Michna <timomichna@yahoo.de>
2011-06-16 16:05:06 +0200

    added test for ReflectionPool

---
+++ tests/util/ReflectionPoolTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once dirname(__FILE__).'/../../misc/bootstrap.php';
+PHPUnit_Util_Fileloader::checkAndLoad(dirname(__FILE__).'/../_files/ReflectionTestResources.php');
+
+
+
+class ReflectionPoolTest extends PHPUnit_Framework_TestCase  {
+
+     public function testGetInstanceReturnsReflectionClass(){
+         $cls = PEIP_Reflection_Pool::getInstance('ReflectionTestClass1');
+         $this->assertTrue($cls instanceof  ReflectionClass);
+         $cls = PEIP_Reflection_Pool::getInstance(new ReflectionTestClass1);
+         $this->assertTrue($cls instanceof  ReflectionClass);
+     }
+
+     public function testGetInstanceCachesReflectionClassInstances(){
+         $cls1 = PEIP_Reflection_Pool::getInstance('ReflectionTestClass1');
+         $cls2 = PEIP_Reflection_Pool::getInstance(new ReflectionTestClass1);
+         $this->assertEquals($cls1, $cls2);
+     }
+}
