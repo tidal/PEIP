b4c4fbc452246bcf95a0c02f940f6a368c6de513
Timo Michna <timomichna@yahoo.de>
2011-06-16 15:51:13 +0200

    minor changes to Reflection

--- src/util/PEIP_Reflection.php
+++ src/util/PEIP_Reflection.php
@@ -14,15 +14,15 @@ class PEIP_Reflection {
     protected static $classInfo = array();
 
     /*
-     * returns implemeted interfaces and (parents) class names as array
+     * returns implemeted interfaces and (parents) class names as array.
      * When second parameter ($store) ist set to FALSE, the class-info will
      * not be cached. This serves some memory (e.g. 500b per requested class),
      * but wil make subsequent calls slower (about 4-5 times). So when you use
-     * this method only once per process, set $store to false - otherwise set
+     * this method only once per process, set $store to FALSE - otherwise set
      * $store to TRUE. Since PEIP is created for performance on heavy usage
      * the default for store is TRUE.
      *
-     * @param  ehestring $name of the class to return info for
+     * @param  string name/instance of the class to return info for
      * @param  boolean wether to store the info for the class internally
      * @return array array with interface/class info for the class
      */
@@ -34,7 +34,7 @@ class PEIP_Reflection {
         }
         $cls = PEIP_Reflection_Pool::getInstance($class);
         // get the names of implemented interfaces
-        $classInfo = $cls->getInterfaceNames();
+        $classInfo = (array)$cls->getInterfaceNames();
         $classInfo[] = $cls->getName();
         // get names of parent-classes
         while($cls = $cls->getParentClass()){
