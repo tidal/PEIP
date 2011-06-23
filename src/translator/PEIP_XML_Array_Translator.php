4d3d1b2946cd52dcb5b4ab254152aa50b6aeddae
Timo Michna <timomichna@yahoo.de>
2011-06-23 17:46:35 +0200

    fixed XmlArrayTranslator

--- src/translator/PEIP_XML_Array_Translator.php
+++ src/translator/PEIP_XML_Array_Translator.php
@@ -48,7 +48,7 @@ class PEIP_XML_Array_Translator {
         foreach($node->children() as $nr=>$child){
             $name = $child->getName();
             $res = self::doTranslate($child);
-            if(!issset($array[$name]) && !is_array($array[$name])){
+            if(!isset($array[$name]) && !is_array($array[$name])){
                 if(is_string($array[$name])){
                     $array[$name] = array(
                         array(
