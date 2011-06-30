b5a2cc91d4c97b5a099fbfddcb6e7ea261158793
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:28:03 +0200

    added wrapper class for header constants

---
+++ src/Constant/Header.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PEIP\Constant;
+
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * PEIP\Constant\Header
+ * Wrapper Class for Header Constants
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ * @subpackage Constant 
+ */
+
+class Header {
+
+    const
+        TIME                    = 'TIME',
+        TIMEOUT                 = 'TIMEOUT',
+        SERVICE                 = 'SERVICE',
+        CONTEXT                 = 'CONTEXT',
+        MESSAGE                 = 'MESSAGE',
+        DISPATCHER              = 'DISPATCHER',
+        LISTENER                = 'LISTENER',
+        EVENT                   = 'EVENT',
+        NODE                    = 'NODE',
+        SENT                    = 'SENT',
+        SUBSCRIBER              = 'SUBSCRIBER',
+        REPLY_CHANNEL           = 'REPLY_CHANNEL',
+        KEY                     = 'KEY',
+        NODE_CONFIG             = 'NODE_CONFIG',
+        NODE_NAME               = 'NODE_NAME',
+        NODE_ID                 = 'NODE_ID',
+        COUNT_CONFIG            = 'COUNT_CONFIG';
+
+}
+
