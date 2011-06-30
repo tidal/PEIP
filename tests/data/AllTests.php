1adbb791359a37c594562e55561d61485cfeef76
Timo Michna <timomichna@yahoo.de>
2011-06-30 20:57:40 +0200

    refactored AllTests to generic suite loader

--- tests/data/AllTests.php
+++ tests/data/AllTests.php
@@ -1,11 +1,19 @@
 <?php
 
-require_once dirname(__FILE__).'/ParameterHolderTest.php';
-
  class Data_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
-		$suite = new PHPUnit_Framework_TestSuite('data');
-		$suite->addTestSuite('ParameterHolderTest');
+        $suite = new PHPUnit_Framework_TestSuite('data');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
 		return $suite;
 	}
 }
