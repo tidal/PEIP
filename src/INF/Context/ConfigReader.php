11071566338aa693c552c8613d178e707dea798e
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:33:00 +0200

    added Interface

---
+++ src/INF/Context/ConfigReader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PEIP\INF\Context;
+
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * \PEIP\INF\Context\ConfigReader
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ * @subpackage context
+ */
+
+interface ConfigReader {
+
+    public function read();
+
+}
+
