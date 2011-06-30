beb9d454b4710715f3c96d5204c04a1a761ee5b4
Timo Michna <timomichna@yahoo.de>
2011-07-01 00:47:20 +0200

    added autoload tests

---
+++ tests/autoload/AllTests.php
@@ -0,0 +1,19 @@
+<?php
+
+ class Autoload_AllTests extends PHPUnit_Framework_TestSuite {
+	 public static function suite(){
+        $suite = new PHPUnit_Framework_TestSuite('autoload');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
+		return $suite;
+	}
+}
