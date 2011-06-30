f9e30781a159a17a4779c45cf5889c7e0c9ba1ab
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:02:34 +0200

    refactored AllTests to generic suite loader

--- tests/message/AllTests.php
+++ tests/message/AllTests.php
@@ -1,17 +1,19 @@
 <?php
 
-require_once dirname(__FILE__).'/CallableMessageHandlerTest.php';
-require_once dirname(__FILE__).'/GenericMessageTest.php';
-require_once dirname(__FILE__).'/MessageBuilderTest.php';
-require_once dirname(__FILE__).'/StringMessageTest.php';
-
  class Message_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
-		$suite = new PHPUnit_Framework_TestSuite('message');
-		$suite->addTestSuite('CallableMessageHandlerTest');
-		$suite->addTestSuite('GenericMessageTest');
-		$suite->addTestSuite('MessageBuilderTest');
-		$suite->addTestSuite('StringMessageTest');
+        $suite = new PHPUnit_Framework_TestSuite('message');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
 		return $suite;
 	}
 }
