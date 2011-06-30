b5ba0110f89c167f8c37c28e42c6b876967be108
Timo Michna <timomichna@yahoo.de>
2011-06-30 20:56:36 +0200

    refactored AllTests to generic suite loader

--- tests/command/AllTests.php
+++ tests/command/AllTests.php
@@ -1,11 +1,19 @@
 <?php
 
-require_once dirname(__FILE__).'/CommandTest.php';
-
  class Command_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
-		$suite = new PHPUnit_Framework_TestSuite('command');
-		$suite->addTestSuite('CommandTest');
+        $suite = new PHPUnit_Framework_TestSuite('command');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
 		return $suite;
 	}
 }
