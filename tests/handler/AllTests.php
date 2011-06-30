b99f0c3828d6adefcacf0d3e35690c48f9beaeb8
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:02:24 +0200

    refactored AllTests to generic suite loader

--- tests/handler/AllTests.php
+++ tests/handler/AllTests.php
@@ -5,8 +5,18 @@ require_once dirname(__FILE__).'/CallableHandlerTest.php';
 
  class Handler_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
-		$suite = new PHPUnit_Framework_TestSuite('message');
-		$suite->addTestSuite('CallableHandlerTest');
+        $suite = new PHPUnit_Framework_TestSuite('handler');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
 		return $suite;
 	}
 }
