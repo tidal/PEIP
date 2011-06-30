b921d7485e9d339c816ad76a378f5088f525e4b6
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:02:13 +0200

    refactored AllTests to generic suite loader

--- tests/translator/AllTests.php
+++ tests/translator/AllTests.php
@@ -1,10 +1,19 @@
 <?php
-require_once dirname(__FILE__).'/XmlArrayTranslatorTest.php';
 
  class Translator_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
-		$suite = new PHPUnit_Framework_TestSuite('translator');
-        $suite->addTestSuite('XmlArrayTranslatorTest');
+        $suite = new PHPUnit_Framework_TestSuite('translator');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            $file = $iterator->current();
+            if($iterator->isFile() && $file != 'AllTests.php'){
+                require_once __DIR__.'/'.$file;
+                $suiteName = str_replace('.php', '', $file);
+                $suite->addTestSuite(str_replace('.php', '', $file));
+            }
+            $iterator->next();
+        }
 		return $suite;
 	}
 }
