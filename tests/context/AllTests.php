75c76042b04659c3df9f806f9413793435779239
Timo Michna <timomichna@yahoo.de>
2011-06-30 20:57:25 +0200

    refactored AllTests to generic suite loader

---
+++ tests/context/AllTests.php
@@ -0,0 +1,17 @@
+<?php
+
+ class Context_AllTests extends PHPUnit_Framework_TestSuite {
+	 public static function suite(){
+        $suite = new PHPUnit_Framework_TestSuite('context');
+        $iterator = new DirectoryIterator(__DIR__);
+        $iterator->rewind();
+        while($iterator->valid()){
+            if($iterator->isFile() && $iterator->current() != 'AllTests.php'){
+                require_once __DIR__.'/'.$iterator->current();
+                $suite->addTestSuite(str_replace('.php', '', $iterator->current()));
+            }
+            $iterator->next();
+        }
+		return $suite;
+	}
+}
