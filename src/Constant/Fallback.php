1a922ad41dafe80982d6972ee1f35f6762e4a3a1
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:28:49 +0200

    added wrapper class for default/fallback constants

---
+++ src/Constant/Fallback.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PEIP\Constant;
+
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * PEIP\Constant\Fallback
+ * Wrapper Class for Fallback/default Constants
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ * @subpackage Constant
+ */
+
+class Fallback {
+
+    const
+        CLASS_MESSAGE_DISPATCHER    = '\PEIP\Dispatcher\Dispatcher',
+        CLASS_EVENT_DISPATCHER      = '\PEIP\Dispatcher\ObjectEventDispatcher',
+        EVENT_CLASS                 = '\PEIP\Event\Event';
+
+}
+
