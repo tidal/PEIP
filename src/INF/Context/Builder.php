efbb7035da02d570b71c76cee93ea5e0c77da42d
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:32:51 +0200

    added Interface

---
+++ src/INF/Context/Builder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PEIP\INF\Context;
+
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * \PEIP\INF\Context\Context
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ * @subpackage context
+ */
+
+class Builder {
+
+    public static function build($config){
+
+
+
+    }
+
+    public static function buildNode($config){
+
+    }
+
+
+}
+
