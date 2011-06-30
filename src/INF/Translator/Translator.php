4902ac3b01cb887eb26fecbf2358a53fb8721d59
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:35:21 +0200

    added Interface

---
+++ src/INF/Translator/Translator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PEIP\INF\Translator;
+
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Translator
+ *
+ * @author timo
+ */
+interface Translator {
+    
+}
+
