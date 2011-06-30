d4cf11904b35d206bab20ea08469c5982e9aa3c7
Timo Michna <timomichna@yahoo.de>
2011-06-30 21:58:16 +0200

    removed redundant namespace

--- src/Channel/ChannelAdapter.php
+++ src/Channel/ChannelAdapter.php
@@ -2,7 +2,22 @@
 
 namespace PEIP\Channel;
 
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+/**
+ * PEIP\Channel\ChannelAdapter
+ * Abstract base class for all service activators
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ * @subpackage Channel
+ */
 
 class ChannelAdapter {
 
@@ -24,10 +39,10 @@ class ChannelAdapter {
 
     public function handle($object){
         $message = $this->getMessage($object);
-                if(!is_object($message)){ 
-                    throw new \Exception('Could not get Message from Channel');
-                }               
-                $this->handler->handle($message);
+        if(!is_object($message)){
+            throw new \Exception('Could not get Message from Channel');
+        }
+        $this->handler->handle($message);
     } 
 
 
