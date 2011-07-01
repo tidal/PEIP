0070c9ab11f2d9d6dad152dd715e2ea982802027
Timo Michna <timomichna@yahoo.de>
2011-07-01 02:33:32 +0200

    added test for Wiretap::construct

---
+++ tests/listener/WireTapTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once dirname(__FILE__).'/../../misc/bootstrap.php';
+
+class WireTapTest
+	extends PHPUnit_Framework_TestCase {
+
+
+    public function setup(){
+        $this->input = new \PEIP\Channel\PublishSubscribeChannel('input');
+        $this->output = new \PEIP\Channel\PollableChannel('output');
+        $this->wiretap = new \PEIP\Listener\Wiretap($this->input, $this->output);
+    }
+
+
+    public function testConstruct(){
+        $this->assertTrue($this->wiretap instanceof \PEIP\Listener\Wiretap);
+        $this->assertSame($this->input, $this->wiretap->getInputChannel());
+        $this->assertSame($this->output, $this->wiretap->getOutputChannel());
+    }
+
+
+
+
+
+
+}
+
