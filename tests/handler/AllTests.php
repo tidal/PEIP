29515b885eb763472f3457c1a20af9cbd19053c2
Timo Michna <timomichna@yahoo.de>
2011-07-01 02:21:29 +0200

    removed include

--- tests/handler/AllTests.php
+++ tests/handler/AllTests.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once dirname(__FILE__).'/CallableHandlerTest.php';
-
-
  class Handler_AllTests extends PHPUnit_Framework_TestSuite {
 	 public static function suite(){
         $suite = new PHPUnit_Framework_TestSuite('handler');
