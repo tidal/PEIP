09356d7e46adffa15a5097899386c55ad5c28383
Timo Michna <timomichna@yahoo.de>
2011-07-22 21:46:46 +0200

    added Queue\Queue

---
+++ src/Queue/Queue.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PEIP\Queue;
+
+/*
+ * This file is part of the PEIP package.
+ * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Queue
+ * Description of Queue
+ *
+ * @author Timo Michna <timomichna/yahoo.de>
+ * @package PEIP
+ */
+
+class Queue 
+    extends \SplQueue
+    implements PEIP\INF\Queue\Queue {
+
+}
