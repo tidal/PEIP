b77b56e31607ba24e2fa7c8790b16e68afe54879
Timo Michna <timomichna@yahoo.de>
2016-06-28 22:50:25 +0200

    updated copyright

--- src/Context/XMLContext.php
+++ src/Context/XMLContext.php
@@ -6,7 +6,7 @@ namespace PEIP\Context;
 
 /*
  * This file is part of the PEIP package.oBuild
- * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ * (c) 2009-2016 Timo Michna <timomichna/yahoo.de>
  * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Listener/Wiretap.php
+++ src/Listener/Wiretap.php
@@ -4,7 +4,7 @@ namespace PEIP\Listener;
 
 /*
  * This file is part of the PEIP package.
- * (c) 2009-2011 Timo Michna <timomichna/yahoo.de>
+ * (c) 2009-2016 Timo Michna <timomichna/yahoo.de>
  * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
